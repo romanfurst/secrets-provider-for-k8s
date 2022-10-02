42cd03d3e52c19dde6e8bf8c80393be2af3b26ce
Furst Roman <roman_furst@kb.cz>
2022-10-03 00:17:36 +0200

    watcher

--- pkg/secrets/provide_conjur_secrets.go
+++ pkg/secrets/provide_conjur_secrets.go
@@ -10,10 +10,12 @@ import (
 
 	"github.com/cyberark/secrets-provider-for-k8s/pkg/log/messages"
 	"github.com/cyberark/secrets-provider-for-k8s/pkg/secrets/clients/conjur"
+	k8sClient "github.com/cyberark/secrets-provider-for-k8s/pkg/secrets/clients/k8s"
 	"github.com/cyberark/secrets-provider-for-k8s/pkg/secrets/config"
 	k8sSecretsStorage "github.com/cyberark/secrets-provider-for-k8s/pkg/secrets/k8s_secrets_storage"
 	"github.com/cyberark/secrets-provider-for-k8s/pkg/secrets/pushtofile"
 	"github.com/cyberark/secrets-provider-for-k8s/pkg/utils"
+	v1 "k8s.io/api/core/v1"
 )
 
 const (
@@ -37,18 +39,23 @@ type ProviderConfig struct {
 // ProviderFunc describes a function type responsible for providing secrets to
 // an unspecified target. It returns either an error, or a flag that indicates
 // whether any target secret files or Kubernetes Secrets have been updated.
-type ProviderFunc func() (updated bool, err error)
+type ProviderFunc func(secrets ...string) (updated bool, err error)
+
+// DeleterFunc delete secret contetnt from provider memory when k8s secret is undeployed
+type DeleterFunc func(secrets []string)
 
 // RepeatableProviderFunc describes a function type that is capable of looping
 // indefinitely while providing secrets to unspecified targets.
 type RepeatableProviderFunc func() error
+
+type WatchSecretFunc func(provider ProviderFunc)
 
 // NewProviderForType returns a ProviderFunc responsible for providing secrets in a given mode.
 func NewProviderForType(
 	traceContext context.Context,
 	secretsRetrieverFunc conjur.RetrieveSecretsFunc,
 	providerConfig ProviderConfig,
-) (ProviderFunc, []error) {
+) (ProviderFunc, DeleterFunc, []error) {
 	switch providerConfig.StoreType {
 	case config.K8s:
 		provider := k8sSecretsStorage.NewProvider(
@@ -57,7 +64,7 @@ func NewProviderForType(
 			providerConfig.CommonProviderConfig.SanitizeEnabled,
 			providerConfig.K8sProviderConfig,
 		)
-		return provider.Provide, nil
+		return provider.Provide, provider.Delete, nil
 	case config.File:
 		provider, err := pushtofile.NewProvider(
 			secretsRetrieverFunc,
@@ -65,12 +72,12 @@ func NewProviderForType(
 			providerConfig.P2FProviderConfig,
 		)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		provider.SetTraceContext(traceContext)
-		return provider.Provide, nil
+		return provider.Provide, nil, nil
 	default:
-		return nil, []error{fmt.Errorf(
+		return nil, nil, []error{fmt.Errorf(
 			messages.CSPFK054E,
 			providerConfig.StoreType,
 		)}
@@ -89,7 +96,7 @@ func RetryableSecretProvider(
 		retryCountLimit,
 	)
 
-	return func() (bool, error) {
+	return func(secrets ...string) (bool, error) {
 		var updated bool
 		var retErr error
 
@@ -97,7 +104,7 @@ func RetryableSecretProvider(
 			if limitedBackOff.RetryCount() > 0 {
 				log.Info(fmt.Sprintf(messages.CSPFK010I, limitedBackOff.RetryCount(), limitedBackOff.RetryLimit))
 			}
-			updated, retErr = provideSecrets()
+			updated, retErr = provideSecrets(secrets...)
 			return retErr
 		}, limitedBackOff)
 
@@ -124,17 +131,19 @@ type ProviderRefreshConfig struct {
 func RepeatableSecretProvider(
 	refreshConfig ProviderRefreshConfig,
 	provideSecrets ProviderFunc,
-	namespace string,
+	deleteSecrets DeleterFunc,
+	k8sProviderConfig k8sSecretsStorage.K8sProviderConfig,
 ) RepeatableProviderFunc {
-	return repeatableSecretProvider(refreshConfig, provideSecrets,
-		defaultStatusUpdater, namespace)
+	return repeatableSecretProvider(refreshConfig, provideSecrets, deleteSecrets,
+		defaultStatusUpdater, k8sProviderConfig)
 }
 
 func repeatableSecretProvider(
 	config ProviderRefreshConfig,
 	provideSecrets ProviderFunc,
+	deleteSecrets DeleterFunc,
 	status statusUpdater,
-	namespace string,
+	k8sProviderConfig k8sSecretsStorage.K8sProviderConfig,
 ) RepeatableProviderFunc {
 
 	var periodicQuit = make(chan struct{})
@@ -170,7 +179,13 @@ func repeatableSecretProvider(
 				periodicQuit:  periodicQuit,
 				periodicError: periodicError,
 			}
-			go periodicSecretProvider(provideSecrets, config, status, k8sProviderConfig.RequiredK8sSecrets...)
+			watcher := k8sClient.K8sSecretWatcher{
+				PodNamespace: k8sProviderConfig.PodNamespace,
+				AddFunc:      watchAddFunc(provideSecrets, status),
+				UpdateFunc:   watchUpdateFunc(provideSecrets, status),
+				DeleteFunc:   watchDeleteFunc(deleteSecrets),
+			}
+			go periodicSecretProvider(provideSecrets, config, status, watcher.Watch, k8sProviderConfig.RequiredK8sSecrets...)
 		default:
 			// Run once and sleep forever if in sidecar mode without
 			// periodic refresh (fall through)
@@ -207,8 +222,12 @@ func periodicSecretProvider(
 	provideSecrets ProviderFunc,
 	config periodicConfig,
 	status statusUpdater,
+	watch k8sClient.WatchK8sSecretFunc,
 	requiredK8sSecrets ...string,
 ) {
+	if requiredK8sSecrets == nil || len(requiredK8sSecrets) < 1 {
+		watch()
+	}
 	for {
 		select {
 		case <-config.periodicQuit:
@@ -223,6 +242,51 @@ func periodicSecretProvider(
 			/*if err != nil {
 				config.periodicError <- err
 			}*/
+		}
+	}
+}
+
+func watchAddFunc(provideSecrets ProviderFunc, status statusUpdater) func(obj interface{}) {
+	return func(obj interface{}) {
+		secretObj, ok := obj.(*v1.Secret)
+		if ok {
+			log.Info("New k8s secret %s created", secretObj.Name)
+			updated, err := provideSecrets(secretObj.Name)
+			if err == nil && updated {
+				log.Info("Newly created Secret %s successfully provided", secretObj.Name)
+				err = status.setSecretsUpdated()
+			}
+		}
+	}
+}
+
+func watchUpdateFunc(provideSecrets ProviderFunc, status statusUpdater) func(oldObj, newObj interface{}) {
+	return func(oldObj, newObj interface{}) {
+		if oldObj == newObj {
+			return
+		}
+		oldSecretObj, ok := newObj.(*v1.Secret)
+		if !ok {
+			return
+		}
+		log.Info("Secret %s updated", oldSecretObj.Name)
+		newSecretObj, ok := newObj.(*v1.Secret)
+		if ok {
+			updated, err := provideSecrets(newSecretObj.Name)
+			if err == nil && updated {
+				log.Info("secret updated")
+				err = status.setSecretsUpdated()
+			}
+		}
+	}
+}
+
+func watchDeleteFunc(deleter DeleterFunc) func(obj interface{}) {
+	return func(obj interface{}) {
+		secretObj, ok := obj.(*v1.Secret)
+		if ok {
+			log.Info("Secret %s deleted", secretObj.Name)
+			deleter([]string{secretObj.Name})
 		}
 	}
 }
